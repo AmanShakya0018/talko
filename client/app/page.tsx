804c20dff30441984d7280c9aaeaba8ea6035327
AmanShakya0018 <amanshakya1808@gmail.com>
2025-04-01 18:05:29 +0530

    feat: implement hero section layout and design

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,11 @@
+import LandingPage from "@/components/landingpage";
 import Navbar from "@/components/Navbar";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Welcome to the Chat App!</h1>
-        <Link href="/room" className="text-blue-500 underline">
-          View All Users
-        </Link>
-      </div>
+      <LandingPage />
     </>
   );
 }
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-// import Image from 'next/image';
+import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
@@ -15,19 +15,19 @@ const Navbar = () => {
   return (
     <nav className="z-50 sticky top-0 w-full bg-secondary/15 shadow-lg shadow-neutral-600/5 backdrop-blur-lg border-b border-primary/10 px-4 lg:px-8
 ">
-      <div className="max-w-[88rem] mx-auto px-4">
+      <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <div className='flex items-center space-x-12'>
             <Link href="/" className="flex items-center">
-              {/* <Image
+              <Image
                 width={500}
                 height={500}
-                src={image}
+                src={"/pfp.png"}
                 alt="talko.png"
                 quality={100}
                 priority={true}
                 className="w-10 h-10 mt-1 rounded-full object-cover flex-shrink-0"
-              /> */}
+              />
               <h3 className="text-2xl font-bold">Talko</h3>
             </Link>
 
